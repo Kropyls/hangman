a1d4d71dbc2066171612a203c2692d946bea5fa3
Kropyls <Kropyls@gmail.com>
2022-07-09 20:53:42 -0700

    Got save/load working. Touched up some methods. Calling this complete for now.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hangman
 Basic hangman game
+
+Plays hangman without a graphic and allows saving and loading of the game
--- hangman.rb
+++ hangman.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+require 'yaml'
 # Game class for the game
 class Game
   def initialize
@@ -7,24 +10,47 @@ class Game
     @current_guess = Array.new(word.length, '_')
     @guesses = ('a'..'z').to_a
     @word = word.split('')
+    @lives = 8
   end
 
   def play
-    count = 8
-    until @current_guess == @word || count.zero?
+    until @current_guess == @word || @lives.zero?
+      puts "Guesses left: #{@lives}"
       puts @current_guess.join(' ')
       letter = player_guess
-      count -= 1 unless check_guess(letter)
-      puts "Guesses left: #{count}"
+      if letter == 'save'
+        save_game('./save.yml')
+        return
+      end
+      @lives -= 1 unless check_guess(letter)
     end
     count.zero? ? (puts "LOSE, word was: #{@word.join('')}") : (puts "WIN! Word was: #{@word.join('')}")
   end
 
+  def save_game(file_path)
+    File.open(file_path, 'w') do |f|
+      f.write(YAML.dump(self))
+    end
+    puts "file save to #{file_path} successfully"
+  end
+
+  def self.load_game(file_path)
+    game = YAML.safe_load(
+      File.read(file_path),
+      permitted_classes: [Game]
+    )
+    File.delete(file_path)
+    game
+  end
+
   private
-  
+
   def player_guess
     puts 'Please enter a letter. Only one letter is allowed and cannot have already been chosen.'
+    puts 'Type "save" to save.'
     letter = gets.chomp.downcase
+    return letter if letter == 'save'
+
     if @guesses.include? letter
       @guesses.delete letter
       return letter
@@ -43,5 +69,22 @@ class Game
   end
 end
 
-g = Game.new
+def ask_to_load_game
+  puts 'found save file. Load it? (y or n)'
+  case gets.chomp.downcase
+  when 'y'
+    Game.load_game('./save.yml')
+  when 'n'
+    Game.new
+  else
+    puts 'Invalid input, try again'
+    ask_to_load_game
+  end
+end
+
+g = if File.file?('./save.yml')
+      ask_to_load_game
+    else
+      Game.new
+    end
 puts g.play
